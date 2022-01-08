a043fbc5c8c83721cdf56d23f601badd1ff9898a
Lucy <lucy@absolucy.moe>
2022-01-08 16:28:58 -0500

    Initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf "$PWD/Cargo/config.toml" "$HOME/.cargo/config.toml"
+ln -sf "$PWD/Git/gitconfig" "$HOME/.gitconfig"
+ln -sf "$PWD/Git/gitignore_global" "$HOME/.gitignore_global"
+ln -sf "$PWD/Shell/profile" "$HOME/.profile"
+ln -sf "$PWD/Shell/zshrc" "$HOME/.zshrc"
