9893d0b40a501ee9575640febab233bd03a7c17b
Lucy <lucy@absolucy.moe>
2022-01-11 13:26:03 -0500

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+mkdir -p "$HOME"/{.cargo,.config/paru}
+
 ln -sf "$PWD/Cargo/config.toml" "$HOME/.cargo/config.toml"
 ln -sf "$PWD/Git/gitconfig" "$HOME/.gitconfig"
 ln -sf "$PWD/Git/gitignore_global" "$HOME/.gitignore_global"
+ln -sf "$PWD/Paru/paru.conf" "$HOME/.config/paru/paru.conf"
 ln -sf "$PWD/Shell/profile" "$HOME/.profile"
 ln -sf "$PWD/Shell/zshrc" "$HOME/.zshrc"
