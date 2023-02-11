d2ff08f647da9ca54c8414b6e955d9321a5d5b41
Lucy <lucy@absolucy.moe>
2023-02-10 19:56:04 -0500

    Improve powershell profile script

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,31 @@
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
-Set-PsFzfOption -TabExpansion
-Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Function Test-CommandExists {
+	Param ($command)
+	$oldPreference = $ErrorActionPreference
+	$ErrorActionPreference = 'stop'
+	try {
+		if (Get-Command $command) {
+			RETURN $true
+		}
+	} Catch {
+		RETURN $false
+	} Finally {
+		$ErrorActionPreference=$oldPreference
+	}
+}
+
+If (Test-CommandExists starship) {
+	Invoke-Expression (&starship init powershell)
+}
+If (Test-CommandExists zoxide) {
+	Invoke-Expression (& {
+		$hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+		(zoxide init --hook $hook powershell | Out-String)
+	})
+}
+If (Test-CommandExists Set-PsFzfOption && Test-CommandExists Set-PSReadLineKeyHandler) {
+	Set-PsFzfOption -TabExpansion
+	Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+}
+If (Test-CommandExists fnm) {
+	fnm env --use-on-cd | Out-String | Invoke-Expression
+}
